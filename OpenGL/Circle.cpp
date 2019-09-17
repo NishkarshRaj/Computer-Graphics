f825c3bf5ffbbcce912eb4e4532ddda6585b7bf0
NishkarshRaj <500060720@stu.upes.ac.in>
2019-09-17 08:50:43 +0530

    Circle Code in Cpp

--- OpenGL/Circle.cpp
+++ OpenGL/Circle.cpp
@@ -1,32 +1,30 @@
+#include<math.h>
+#include<stdio.h>
 #include<GL/glut.h>
 #include<GL/gl.h>
-#include<iostream>
-#include<math.h>
-using namespace std;
-void init()
+
+void display(void)
 {
-glClearColor(0.0,0.0,1.0,1.0); //Blue background
-glMatrixMode(GL_PROJECTION);
-gluOrtho2D(0,700,0,700);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0,1.0,0.0);
+ 	glBegin(GL_POINTS);
+ 	for(int i=0;i<1000;++i)
+  	{
+ 	 glVertex3f(cos(2*3.14159*i/1000.0),sin(2*3.14159*i/1000.0),0);
+ 	 }
+	 glEnd();
+	glFlush();
 }
-void display()
+int main(int argc,char **argv,char **arg)
 {
-glColor3f(0,1,0);
- glBegin(GL_POINTS);
- for(int i=0;i<1000;++i)
-  {
-  glVertex3f(cos(2*3.14159*i/1000.0),sin(2*3.14159*i/1000.0),0);
-  }
- glEnd();
-glFlush();
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowPosition(700,700);
+	glutInitWindowSize(500,500);
+	glutCreateWindow("Circle Code");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+
 }
-int main(int argc,char **argv)
-{
-glutInit(&argc,argv);
-glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-glutInitWindowSize(700,700);
-glutCreateWindow("Circle");
-init();
-glutDisplayFunc(display);
-glutMainLoop();
-}
+
