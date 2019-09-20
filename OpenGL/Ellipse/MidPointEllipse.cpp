f3f042362bca11d8d484c53d1f3b1db5c1c03ffd
NishkarshRaj <500060720@stu.upes.ac.in>
2019-09-20 11:55:53 +0530

    Mid Point Ellipse completed

--- OpenGL/Ellipse/MidPointEllipse.cpp
+++ OpenGL/Ellipse/MidPointEllipse.cpp
@@ -12,19 +12,28 @@ gluOrtho2D(0,700,0,700);
 void display()
 {
 /////////////////
-	int c1,c2,x,y,p1,p2;
+	int c1,c2,x,y,p1,p2,x1,y1,x2,y2;
 	c1 = 0;
 	x = 0;
+x1=x+350;
 	y = ry;
+y1=y+350;
+
 	p1 = (ry*ry) - (rx*rx)*ry + ((rx*rx)/4);
-	glColor3f(0,1,0);
+x2=700-x1;
+y2=700-y1;
+glColor3f(0,1,0);
 glBegin(GL_POINTS);
-glVertex2d(x,y);
+glVertex2d(x1,y1);
+glVertex2d(x1,y2);
+glVertex2d(x2,y1);
+glVertex2d(x2,y2);
 glEnd();
 glFlush();
 	while((ry*ry*x)<=(rx*rx*y))
 	{
 		x = x + 1;
+x1++;
 		if(p1<0)
 		{
 			//y remains same
@@ -34,36 +43,54 @@ glFlush();
 		{
 			y = y-1;
 			p1 = p1 + (ry*ry*(2*x+1)) - 2*(rx*rx)*(y);
+y1--;
 		}
-			glColor3f(0,1,0);
+	x2=700-x1;
+y2=700-y1;
+glColor3f(0,1,0);
 glBegin(GL_POINTS);
-glVertex2d(x,y);
+glVertex2d(x1,y1);
+glVertex2d(x1,y2);
+glVertex2d(x2,y1);
+glVertex2d(x2,y2);
 glEnd();
 glFlush();
 	}
 	// Starting Region 2
 	c2 = 0;
 	p2 =  (ry*ry)*(x+0.5)*(x+0.5) + (rx*rx)*(y-1)*(y-1) - (rx*rx*ry*ry);
-	glColor3f(0,1,0);
+	x2=700-x1;
+y2=700-y1;
+glColor3f(0,1,0);
 glBegin(GL_POINTS);
-glVertex2d(x,y);
+glVertex2d(x1,y1);
+glVertex2d(x1,y2);
+glVertex2d(x2,y1);
+glVertex2d(x2,y2);
 glEnd();
 glFlush();
 	while((y>0)&&(x<=rx))
 	{
 		y = y-1;
+y1--;
 		if(p2<0)
 		{
 			x = x + 1;
+x1++;
 			p2 = p2 + (rx*rx)*(1-2*y) + 2*(ry*ry)*x;
 		}
 		else
 		{
 			p2 = p2 + (rx*rx)*(1-2*y);
 		}
-		glColor3f(0,1,0);
+	x2=700-x1;
+y2=700-y1;
+glColor3f(0,1,0);
 glBegin(GL_POINTS);
-glVertex2d(x,y);
+glVertex2d(x1,y1);
+glVertex2d(x1,y2);
+glVertex2d(x2,y1);
+glVertex2d(x2,y2);
 glEnd();
 glFlush();
 	}
