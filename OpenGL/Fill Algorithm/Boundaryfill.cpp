f4fc8c6e82425121505a196cf6195ed656d5bbbb
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-09-24 09:31:08 +0530

    Boundary Fill Updated

--- OpenGL/Fill Algorithm/Boundaryfill.cpp
+++ OpenGL/Fill Algorithm/Boundaryfill.cpp
@@ -1,43 +1,51 @@
 #include <math.h>
 #include <GL/glut.h>
-#include<GL/gl.h>
+#include <GL/gl.h>
 
-struct Point {
+//Structure 1: Point
+struct Point 
+{
 	GLint x;
 	GLint y;
 };
 
+//Structure 2: Color
 struct Color {
 	GLfloat r;
 	GLfloat g;
 	GLfloat b;
 };
 
+//Function 1: Initialize the OpenGL environment
 void init() {
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glColor3f(0.0, 0.0, 0.0);
-	glPointSize(1.0);
+	glClearColor(1.0, 1.0, 1.0, 0.0); // Black Color
+	glColor3f(0.0, 0.0, 0.0); // White Color
+	glPointSize(1.0); // Specify the point size
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0, 640, 0, 480);
+	gluOrtho2D(0, 700, 0, 700);
 }
 
 // Function of return type Color which is a structure defined above
-Color getPixelColor(GLint x, GLint y) {
-	Color color;
-	glReadPixels(x, y, 1, 1, GL_RGB, GL_FLOAT, &color);
+// Function 2: GetPixelColor
+Color getPixelColor(GLint x, GLint y) 
+{
+	Color color; //Color Structure variable
+	glReadPixels(x, y, 1, 1, GL_RGB, GL_FLOAT, &color); //Built in functions
 	return color;
 }
 
-void setPixelColor(GLint x, GLint y, Color color) {
+// Function 3: SetPixelColor
+void setPixelColor(GLint x, GLint y, Color color) 
+{
 	glColor3f(color.r, color.g, color.b);
 	glBegin(GL_POINTS);
 	glVertex2i(x, y);
 	glEnd();
 	glFlush();
-
 }
 
+// Function 4: Boundary Fill
 void BoundaryFill(int x, int y, Color fillColor, Color boundaryColor) {
 	Color currentColor = getPixelColor(x, y);
 	if(currentColor.r != boundaryColor.r && currentColor.g != boundaryColor.g && currentColor.b != boundaryColor.b) {
@@ -49,59 +57,45 @@ void BoundaryFill(int x, int y, Color fillColor, Color boundaryColor) {
 	}
 }
 
+//Function 5: Mouse Click Function
 void onMouseClick(int button, int state, int x, int y)
 {
 	Color fillColor = {1.0f, 0.0f, 0.0f};		// red color will be filled
 	Color boundaryColor = {0.0f, 0.0f, 0.0f}; // black- boundary
 
-	Point p = {321, 241}; // a point inside the square
+	Point p = {350,350}; // a point inside the square
 
 	BoundaryFill(p.x, p.y, fillColor, boundaryColor);
 }
 
-void draw_dda(Point p1, Point p2) {
-	GLfloat dx = p2.x - p1.x;
-	GLfloat dy = p2.y - p1.y;
-
-	GLfloat x1 = p1.x;
-	GLfloat y1 = p1.y;
-
-	GLfloat step = 0;
-
-	if(abs(dx) > abs(dy)) {
-		step = abs(dx);
-	} else {
-		step = abs(dy);
-	}
-
-	GLfloat xInc = dx/step;
-	GLfloat yInc = dy/step;
-
-	for(float i = 1; i <= step; i++) {
-		glVertex2i(x1, y1);
-		x1 += xInc;
-		y1 += yInc;
-	}
-}
-
-void draw_square(Point a, GLint length) {
-	Point b = {a.x + length, a.y},
-		c = {b.x,	b.y+length},
-		d = {c.x-length, c.y};
-
-	draw_dda(a, b);
-	draw_dda(b, c);
-	draw_dda(c, d);
-	draw_dda(d, a);	
-}
 
 void display(void) {
 	Point pt = {320, 240};
 	GLfloat length = 50;
-
+	int i;
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBegin(GL_POINTS);
-		draw_square(pt, length);
+	//Draw square using points
+	//Lower point of square
+	for(i=200;i<=500;i++)
+	{
+		glVertex2i(i,200);
+	}
+	//right boundary
+	for(i=200;i<=500;i++)
+	{
+		glVertex2i(500,i);
+	}
+	//Top boundary
+	for(i=500;i>=200;i++)
+	{
+		glVertex2i(i,500);
+	}
+	//Left boundary
+	for(i=500;i>=200;i++)
+	{
+		glVertex2i(200,i);
+	}
 	glEnd();
 	glFlush();
 }
@@ -110,9 +104,9 @@ int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-	glutInitWindowSize(640, 480);
+	glutInitWindowSize(700, 700);
 	glutInitWindowPosition(200, 200);
-	glutCreateWindow("Open GL");
+	glutCreateWindow("Boundary Fill : Nishkarsh");
 	init();
 	glutDisplayFunc(display);
 	glutMouseFunc(onMouseClick);
