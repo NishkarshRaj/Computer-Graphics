ca7a0e901a398179485eda82bb402b5e23c3ccd3
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-09-27 12:19:46 +0530

    Update Rishabh.cpp

--- OpenGL/Rishabh.cpp
+++ OpenGL/Rishabh.cpp
@@ -1,6 +1,7 @@
 #include<GL/glut.h>
 #include<GL/gl.h> 
 #include<iostream>
+int x1,y1;
 using namespace std;
 void init()
 {
@@ -9,14 +10,16 @@ void init()
     gluOrtho2D(0.0, 700, 0.0, 700);
 }
 void display()
-{
+{	
+	x1=100;
+	y1=100;
 	glClear( GL_COLOR_BUFFER_BIT );
 	glColor3f(0, 0, 0.0);
 	glBegin( GL_POLYGON );
-		glVertex2i( 100,100 );
-		glVertex2i( 150,100 );
-		glVertex2i( 150,150 );
-		glVertex2i( 100,150 ); 
+		glVertex2i( x1,y1 );
+		glVertex2i( x1+50,y1 );
+		glVertex2i( x1+50,y1+50 );
+		glVertex2i( x1,y1+50 ); 
 	glFlush();
 	glEnd();
 	glColor3f(1,0,0);
@@ -27,6 +30,87 @@ void display()
 
 	glFlush();
 }
+//Keyboard function
+void event1(unsigned char key, int x, int y )
+{
+if(key=='w')
+{
+glColor3f(1, 1, 1);
+	glBegin( GL_POLYGON );
+		glVertex2i( x1,y1 );
+		glVertex2i( x1+50,y1 );
+		glVertex2i( x1+50,y1+50 );
+		glVertex2i( x1,y1+50 ); 
+	glFlush();
+	
+	y1=y1+10;
+	glColor3f(0, 0, 0);
+	glBegin( GL_POLYGON );
+		glVertex2i( x1,y1 );
+		glVertex2i( x1+50,y1);
+		glVertex2i( x1+50,y1+50 );
+		glVertex2i( x1,y1+50 ); 
+	glFlush();
+}
+if(key=='s')
+{
+	glColor3f(1, 1, 1);
+	glBegin( GL_POLYGON );
+		glVertex2i( x1,y1 );
+		glVertex2i( x1+50,y1 );
+		glVertex2i( x1+50,y1+50 );
+		glVertex2i( x1,y1+50 ); 
+	glFlush();
+	
+	y1=y1-10;
+	glColor3f(0, 0, 0);
+	glBegin( GL_POLYGON );
+		glVertex2i( x1,y1 );
+		glVertex2i( x1+50,y1);
+		glVertex2i( x1+50,y1+50 );
+		glVertex2i( x1,y1+50 ); 
+	glFlush();
+}
+if(key=='a')
+{
+glColor3f(1, 1, 1);
+	glBegin( GL_POLYGON );
+		glVertex2i( x1,y1 );
+		glVertex2i( x1+50,y1 );
+		glVertex2i( x1+50,y1+50 );
+		glVertex2i( x1,y1+50 ); 
+	glFlush();
+	
+	x1=x1-10;
+	glColor3f(0, 0, 0);
+	glBegin( GL_POLYGON );
+		glVertex2i( x1,y1 );
+		glVertex2i( x1+50,y1);
+		glVertex2i( x1+50,y1+50 );
+		glVertex2i( x1,y1+50 ); 
+	glFlush();
+}
+if(key=='d')
+{
+	glColor3f(1, 1, 1);
+	glBegin( GL_POLYGON );
+		glVertex2i( x1,y1 );
+		glVertex2i( x1+50,y1 );
+		glVertex2i( x1+50,y1+50 );
+		glVertex2i( x1,y1+50 ); 
+	glFlush();
+	
+	x1=x1+10;
+	glColor3f(0, 0, 0);
+	glBegin( GL_POLYGON );
+		glVertex2i( x1,y1 );
+		glVertex2i( x1+50,y1);
+		glVertex2i( x1+50,y1+50 );
+		glVertex2i( x1,y1+50 ); 
+	glFlush();
+}
+}
+//
 int main(int argc, char**argv) 
 {
     glutInit(&argc, argv); 
@@ -35,5 +119,6 @@ int main(int argc, char**argv)
     glutCreateWindow("Square Line Interaction"); 
     init();  
     glutDisplayFunc(display); 
+    glutKeyboardFunc(event1);
     glutMainLoop();  
 }
