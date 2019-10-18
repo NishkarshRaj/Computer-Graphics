3cb23cbf06a5219148f173d6c74526f893b98251
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-10-18 19:24:25 +0530

    Reflect about y axis

---
+++ 2-D Transformations/Reflect_y.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	cout<<"Reflection about y-axis\n";
+	cout<<"Enter x: ";
+	cin>>x;
+	cout<<"Enter y: ";
+	cin>>y;
+	cout<<"After reflection"<<endl;
+	cout<<"X: "<<x*(-1)<<endl;
+	cout<<"Y: "<<y<<endl;	
+}
