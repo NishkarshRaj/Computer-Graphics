f0b7d0d4053040509f8547fbdfa24fff20aa4fb3
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-11-08 11:20:35 +0530

    Region 1 solved

--- Clipping Algorithms/RegionCode.cpp
+++ Clipping Algorithms/RegionCode.cpp
@@ -3,9 +3,10 @@ using namespace std;
 int main()
 {
 	int i;
+	float m;
 	int xmin,xmax,ymin,ymax; //window
-	int x1,y1; //first end point
-	int x2,y2; //second end point
+	float x1,y1; //first end point
+	float x2,y2; //second end point
 	int p1[4],p2[4]; //region codes
 	cin>>xmin>>xmax>>ymin>>ymax;
 	cin>>x1>>y1;
@@ -108,4 +109,30 @@ int main()
 	{
 		cout<<p2[i]<<" ";
 	}
+	//Clipping
+	m=(y2-y1)/(x2-x1);
+	if(p1[0]==1)
+	{
+		y1=ymax;
+		x1=x1+(ymax-y1)*m;
+	}
+	if(p1[1]==1)
+	{
+		y1=ymin;
+		x1=x1+(ymin-y1)*m;
+	}
+	if(p1[2]==1)
+	{
+		x1=xmax;
+		y1=y1+(xmax-x1)*m;
+	}
+	if(p1[3]==1)
+	{
+		x1=xmin;
+		y1=y1+(xmin-x1)*m;
+	}
+	//region2
+	//output
+	cout<<x1<<" "<<y1<<endl;
+	cout<<x2<<" "<<y2<<endl;
 }
