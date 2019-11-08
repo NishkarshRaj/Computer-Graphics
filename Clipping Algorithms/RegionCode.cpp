a7cbcd0f5ed203aaa4bc2ed32729c944281593fb
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-11-08 11:34:10 +0530

    Cohen Sutherland code completed

--- Clipping Algorithms/RegionCode.cpp
+++ Clipping Algorithms/RegionCode.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	int i;
 	float m;
-	int xmin,xmax,ymin,ymax; //window
+	float xmin,xmax,ymin,ymax; //window
 	float x1,y1; //first end point
 	float x2,y2; //second end point
 	int p1[4],p2[4]; //region codes
@@ -113,26 +113,55 @@ int main()
 	m=(y2-y1)/(x2-x1);
 	if(p1[0]==1)
 	{
+		
+		x1=x1+((ymax-y1)*m);
 		y1=ymax;
-		x1=x1+(ymax-y1)*m;
 	}
 	if(p1[1]==1)
 	{
+		
+		x1=x1+((ymin-y1)*m);
 		y1=ymin;
-		x1=x1+(ymin-y1)*m;
 	}
 	if(p1[2]==1)
 	{
+		
+		y1=y1+((xmax-x1)*m);
 		x1=xmax;
-		y1=y1+(xmax-x1)*m;
 	}
 	if(p1[3]==1)
 	{
+		
+		y1=y1+((xmin-x1)*m);
 		x1=xmin;
-		y1=y1+(xmin-x1)*m;
 	}
 	//region2
+	if(p2[0]==1)
+	{
+		
+		x2=x2+((ymax-y2)*m);
+		y2=ymax;
+	}
+	if(p2[1]==1)
+	{
+		
+		x2=x2+((ymin-y2)*m);
+		y2=ymin;
+	}
+	if(p2[2]==1)
+	{
+		
+		y2=y2+((xmax-x2)*m);
+		x2=xmax;
+	}
+	if(p2[3]==1)
+	{
+		y2=y2+((xmin-x2)*m);
+		x2=xmin;
+	}
 	//output
+	cout<<endl;
 	cout<<x1<<" "<<y1<<endl;
 	cout<<x2<<" "<<y2<<endl;
+	cout<<m<<endl;
 }
