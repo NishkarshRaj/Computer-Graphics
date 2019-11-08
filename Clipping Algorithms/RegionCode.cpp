cf1c9c7e69bbdbb21541ad7ef1d3047b395f1433
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-11-08 10:54:33 +0530

    Region code completed

--- Clipping Algorithms/RegionCode.cpp
+++ Clipping Algorithms/RegionCode.cpp
@@ -7,6 +7,9 @@ int main()
 	int x1,y1; //first end point
 	int x2,y2; //second end point
 	int p1[4],p2[4]; //region codes
+	cin>>xmin>>xmax>>ymin>>ymax;
+	cin>>x1>>y1;
+	cin>>x2>>y2;
 	//initialize region codes
 	for(i=0;i<4;i++)
 	{
@@ -34,5 +37,75 @@ int main()
 	{
 		p1[1]=1;
 	}
-	
+	else if((x1<xmin)&&(x1<xmax)&&(y1>ymin)&&(y1>ymax))
+	{
+		p1[0]=1;
+		p1[3]=1;
+	}
+	else if((x1>xmin)&&(x1>xmax)&&(y1>ymin)&&(y1>ymax))
+	{
+		p1[0]=1;
+		p1[2]=1;
+	}
+	else if((x1<xmin)&&(x1<xmax)&&(y1<ymin)&&(y1<ymax))
+	{
+		p1[1]=1;
+		p1[3]=1;
+	}
+	else if((x1>xmin)&&(x1>xmax)&&(y1<ymin)&&(y1<ymax))
+	{
+		p1[1]=1;
+		p1[2]=1;
+	}
+	//Region Code for p2
+	if((x1>xmin)&&(x1<xmax)&&(y1>ymin)&&(y1<ymax))
+	{
+		//
+	}
+	else if((x2<xmin)&&(x2<xmax)&&(y2>ymin)&&(y2<ymax)) 
+	{
+		p2[3]=1;
+	}
+	else if((x2>xmin)&&(x2>xmax)&&(y2>ymin)&&(y2<ymax))
+	{
+		p2[2]=1;
+	}
+	else if((x2>xmin)&&(x2<xmax)&&(y2>ymin)&&(y2>ymax))
+	{
+		p2[0]=1;
+	}
+	else if((x2>xmin)&&(x2<xmax)&&(y2<ymin)&&(y2<ymax))
+	{
+		p2[1]=1;
+	}
+	else if((x2<xmin)&&(x2<xmax)&&(y2>ymin)&&(y2>ymax))
+	{
+		p2[0]=1;
+		p2[3]=1;
+	}
+	else if((x2>xmin)&&(x2>xmax)&&(y2>ymin)&&(y2>ymax))
+	{
+		p2[0]=1;
+		p2[2]=1;
+	}
+	else if((x2<xmin)&&(x2<xmax)&&(y2<ymin)&&(y2<ymax))
+	{
+		p2[1]=1;
+		p2[3]=1;
+	}
+	else if((x2>xmin)&&(x2>xmax)&&(y2<ymin)&&(y2<ymax))
+	{
+		p2[1]=1;
+		p2[2]=1;
+	}
+	// print region codes
+	for(i=0;i<4;i++)
+	{
+		cout<<p1[i]<<" ";
+	}
+	cout<<endl;
+	for(i=0;i<4;i++)
+	{
+		cout<<p2[i]<<" ";
+	}
 }
