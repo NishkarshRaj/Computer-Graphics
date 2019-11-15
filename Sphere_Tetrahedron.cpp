9c3989ad747cd3fd0cdc9e6a608fccf0a258166e
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-11-15 11:11:44 +0530

    Trying Sphere

--- Sphere_Tetrahedron.cpp
+++ Sphere_Tetrahedron.cpp
@@ -3,40 +3,91 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-void init()
-{
-glClearColor(0.0,0.0,0.0,1.0); //Blue background
-glMatrixMode(GL_PROJECTION);
-gluOrtho2D(0,700,0,700);
+char title[] = "3D Shapes";
+void initGL() {
+   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+   glClearDepth(1.0f);                 
+   glEnable(GL_DEPTH_TEST);  
+   glDepthFunc(GL_LEQUAL);   
+   glShadeModel(GL_SMOOTH);  
+   glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  
 }
-void display()
-{
-glClear(GL_COLOR_BUFFER_BIT);
-//TetraHedron White
-//Polygon comprising of 4 points in 3 Dimension
-//Thus we are using Vertex "3" for 3 Dimension
-glColor3f(1,1,1);
-glBegin(GL_POLYGON);
-glVertex3i(40,50,60);
-glVertex3i(250,250,250);
-glVertex3i(0,0,0);
-glVertex3i(500,500,500);
-glEnd();
-glFlush();
-//Sphere -> Red
-/*glColor3f(1,0,0);
-glBegin(GL_POINTS)
-glEnd();
-glFlush();
-*/
+void display() {
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();                  // Reset the model-view matrix
+   glTranslatef(-1.5f, 0.0f, -6.0f);  // Move left and into the screen
+ /////////////////////////////////// Sphere
+ int i;
+ glBegin(GL_POINTS);                
+ 		//YZ Circle
+      glColor3f(1.0f, 1.0f, 1.0f); 
+       	for(i=0;i<1000;++i)
+  	 {
+ 	 glVertex3f(0,cos(2*3.14159*i/1000.0),sin(2*3.14159*i/1000.0));
+ 	 }
+
+ 		
+ 		//XY-Circle
+      glColor3f(1.0f, 0.0f, 0.0f);  
+      for(i=0;i<1000;++i)
+  	 {
+ 	 glVertex3f(cos(2*3.14159*i/1000.0),sin(2*3.14159*i/1000.0),0);
+ 	 }
+ 
+ 		//XZ Circle
+      glColor3f(1.0f, 0.0f, 0.0f);     // Red
+      for(i=0;i<1000;++i)
+  	 {
+ 	 glVertex3f(cos(2*3.14159*i/1000.0),0,sin(2*3.14159*i/1000.0));
+ 	 }
+ glEnd();
+ ///////////////////////////////////
+   glBegin(GL_TRIANGLES);           
+      glColor3f(1.0f, 0.0f, 0.0f);     // Red
+      glVertex3f( 0.0f, 1.0f, 0.0f);
+      glColor3f(1.0f, 1.0f, 1.0f);   
+      glVertex3f(-1.0f, -1.0f, 1.0f);
+      glColor3f(1.0f, 0.0f, 0.0f);   
+      glVertex3f(1.0f, -1.0f, 1.0f);
+      glColor3f(1.0f, 1.0f, 1.0f);   
+      glVertex3f(0.0f, 1.0f, 0.0f);
+      glColor3f(1.0f, 0.0f, 0.0f);   
+      glVertex3f(1.0f, -1.0f, 1.0f);
+      glColor3f(1.0f, 0.0f, 0.0f);  
+      glVertex3f(1.0f, -1.0f, -1.0f);
+      glColor3f(1.0f, 0.0f, 0.0f);     // Red
+      glVertex3f(0.0f, 1.0f, 0.0f);
+      glColor3f(1.0f, 0.0f, 0.0f);
+      glVertex3f(1.0f, -1.0f, -1.0f);
+      glColor3f(1.0f, 1.0f, 1.0f);
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+      glColor3f(1.0f,0.0f,0.0f);       // Red
+      glVertex3f( 0.0f, 1.0f, 0.0f);
+      glColor3f(1.0f,0.0f,0.0f);  
+      glVertex3f(-1.0f,-1.0f,-1.0f);
+      glColor3f(1.0f,1.0f,1.0f);    
+      glVertex3f(-1.0f,-1.0f, 1.0f);
+   glEnd();   
+   glutSwapBuffers();
 }
-int main(int argc,char **argv)
-{
-glutInit(&argc,argv);
-glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-glutInitWindowSize(700,700);
-glutCreateWindow("Lab Programming Test");
-init();
-glutDisplayFunc(display);
-glutMainLoop();
+void reshape(GLsizei width, GLsizei height) {  
+   if (height == 0) height = 1;                
+   GLfloat aspect = (GLfloat)width / (GLfloat)height;
+   glViewport(0, 0, width, height);
+   glMatrixMode(GL_PROJECTION); 
+   glLoadIdentity();            
+   gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+}
+int main(int argc, char** argv) {
+   glutInit(&argc, argv);            
+   glutInitDisplayMode(GLUT_DOUBLE); 
+   glutInitWindowSize(640, 480);   
+   glutInitWindowPosition(50, 50); 
+   glutCreateWindow(title);        
+   glutDisplayFunc(display);       
+   glutReshapeFunc(reshape);     
+   initGL();       
+   glutMainLoop(); 
+   return 0;
 }
