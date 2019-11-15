6bf3a1bbb545bef023ae3111bae79a476c859bc0
NishkarshRaj <500060720@stu.upes.ac.in>
2019-11-15 10:51:26 +0530

    Update

--- Sphere_Tetrahedron.cpp
+++ Sphere_Tetrahedron.cpp
@@ -5,27 +5,30 @@
 using namespace std;
 void init()
 {
-glClearColor(0.0,0.0,1.0,1.0); //Blue background
+glClearColor(0.0,0.0,0.0,1.0); //Blue background
 glMatrixMode(GL_PROJECTION);
 gluOrtho2D(0,700,0,700);
 }
 void display()
 {
 glClear(GL_COLOR_BUFFER_BIT);
-/*glColor3f(0,1,0);
+//TetraHedron White
+//Polygon comprising of 4 points in 3 Dimension
+//Thus we are using Vertex "3" for 3 Dimension
+glColor3f(1,1,1);
 glBegin(GL_POLYGON);
-glVertex2i(0,0);
-glVertex2i(700,0);
-glVertex2i(700,40);
-glVertex2i(0,40);
+glVertex3i(40,50,60);
+glVertex3i(250,250,250);
+glVertex3i(0,0,0);
+glVertex3i(500,500,500);
 glEnd();
 glFlush();
-*/
 //Sphere -> Red
-glColor3f(1,0,0);
+/*glColor3f(1,0,0);
 glBegin(GL_POINTS)
 glEnd();
 glFlush();
+*/
 }
 int main(int argc,char **argv)
 {
