b2b44880b69d6b534284b5479bf4b17b96e41448
Joshua Norman <jmnorma@clemson.edu>
2020-03-11 10:55:26 -0400

    BUGFIX: integer division

--- OpenGL/Fractals/mandelbrot.cpp
+++ OpenGL/Fractals/mandelbrot.cpp
@@ -47,7 +47,7 @@ void mandelbrot_construction( int left, int right, int top, int bottom){
             std::complex<double> comp( C_a, C_b ); 
 
             int iteration = mandelbrot_formula(comp);
-            double color = 1 - (double)(iteration/ MAX_VALUE);
+            double color = 1 - ((double)iteration/ MAX_VALUE);
 
             glColor3f(color,color,color);
             glVertex2i(b,c); 
